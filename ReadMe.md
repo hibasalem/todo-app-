e754f84e76269bb4196321efef2f5c5b6ae3fefc
hibasalem <salemhiba.hs@gmail.com>
2021-08-15 16:29:03 +0300

    started

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# To Do List Manager
+
+A Web Application for securely managing a To Do List
+
+- **_Phase 1_**: Application Setup
+  Basic To Do List Management, using Hooks
+
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import ToDo from './components/todo/Todo.js';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <ToDo />
+    );
+  }
+}
